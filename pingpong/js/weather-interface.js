fd05ac8e957ebedd6129c412445bc0fe5f48cab7
Andrew Malkin and Nathaniel Meyer <andrew@>
2017-04-18 11:18:08 -0700

    create .env for apiKey and other sensitive data

--- pingpong/js/weather-interface.js
+++ pingpong/js/weather-interface.js
@@ -1,13 +1,18 @@
-var apiKey = "4309f99993444275baed8fc2fee56a75";
+var apiKey = require('./../.env').apiKey;
 
 $(document).ready(function() {
   $('#weather-location').click(function() {
     var city = $('#location').val();
     $('#location').val("");
-    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
-      // console.log(response);
-      // console.log(JSON.stringify(response));
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey)
+     .then(function(response) {
       $('.showWeather').text("The humidity in " + response.name + " is " + response.main.humidity + "%");
-    });
+      console.log(response);
+    })
+     .fail(function(error) {
+       $('.showWeather').text(error.responseJSON.message);
+       console.log(error);
+     });
+    console.log("Notice: The GET request has been made.");
   });
 });
