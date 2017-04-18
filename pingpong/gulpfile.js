12a8dd7ed22cf12d71b1acf5c50b00e6c82f9bf6
Andrew Malkin and Nathaniel Meyer <andrew@>
2017-04-18 09:17:57 -0700

    Add dev server with browserSync

--- pingpong/gulpfile.js
+++ pingpong/gulpfile.js
@@ -6,6 +6,7 @@ var uglify = require('gulp-uglify');
 var utilities = require('gulp-util');
 var del = require('del');
 var jshint = require('gulp-jshint');
+var browserSync = require('browser-sync').create();
 var buildProduction = utilities.env.production;
 var lib = require('bower-files')({
   "overrides":{
@@ -70,4 +71,24 @@ gulp.task('jshint', function(){
   return gulp.src(['js/*.js'])
   .pipe(jshint())
   .pipe(jshint.reporter('default'));
+});
+
+gulp.task('serve', function() {
+  browserSync.init({
+    server: {
+      baseDir: "./",
+      index: "index.html"
+    }
+  });
+
+  gulp.watch(['js/*.js'], ['jsBuild'])
+  gulp.watch(['bower.json'], ['bowerBuild']);
+});
+
+gulp.task('jsBuild', ['jsBrowserify', 'jshint'], function(){
+  browserSync.reload();
+});
+
+gulp.task('bowerBuild', ['bower'], function(){
+  browserSync.reload();
 });
